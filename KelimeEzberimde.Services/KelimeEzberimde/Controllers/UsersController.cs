da3c25e63fba21704788480f2553b005fa756ce3
fatihdemirtc <fatihdemirtc@gmail.com>
2017-03-26 01:20:19 +0300

    servis

---
+++ KelimeEzberimde.Services/KelimeEzberimde/Controllers/UsersController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+
+namespace KelimeEzberimde.Controllers
+{
+    public class UsersController : ApiController
+    {
+        KelimeEzberimdeEntities db = new KelimeEzberimdeEntities();
+
+        public void SaveUser(User posting)
+        {
+            db.Users.Add(posting);
+            db.SaveChanges();
+        }
+
+        public IEnumerable<User> Get()
+        {
+            return db.Users.ToList();
+        }
+        public IEnumerable<User> Get(int id)
+        {
+            return db.Users.Where(x => x.Id == id);
+        }
+
+    }
+}
---
+++ KelimeEzberimde.Services/KelimeEzberimde/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="KelimeEzberimde.Global" Language="C#" %>
